51bbe0f7e4ce20527640e4f510a30b640702d204
Lígia Sell <ligia.sell@hotmail.com>
2022-09-12 18:15:05 -0300

    add redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,23 @@
 import { compose, createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import { rootReducer } from './root-reducer'
 
-const middleWares = [logger]
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'], // Values that you don't want to persist to not conflict with local storage
+  // whitelist: [], Values that you want to persist
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean)
 
 const composedEnhancers = compose(applyMiddleware(...middleWares))
 
-export const store = createStore(rootReducer, undefined, composedEnhancers)
+export const store = createStore(persistedReducer, undefined, composedEnhancers)
+
+export const persistor = persistStore(store)
